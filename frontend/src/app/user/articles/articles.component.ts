90e53806c114b3bd2cd44cb289b4bcb056873d07
Anas Bensaid <bensaidanass9@gmail.com>
2023-07-17 20:51:04 +0100

    Adding news to the user

--- frontend/src/app/user/articles/articles.component.ts
+++ frontend/src/app/user/articles/articles.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { News } from 'src/app/models/news';
+import { NewsService } from 'src/app/services/news.service';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss']
 })
-export class ArticlesComponent {
+export class ArticlesComponent implements OnInit {
+  newsList: News[] = [];
 
+  constructor(private newsService: NewsService) { }
+
+  ngOnInit(): void {
+    this.fetchNews();
+  }
+
+  fetchNews(): void {
+    this.newsService.getNews()
+      .subscribe(
+        newsList => {
+          this.newsList = newsList;
+        },
+        error => {
+          console.error('An error occurred:', error);
+        }
+      );
+  }
+
+  newsDetails(news: News) {
+
+  }
 }
